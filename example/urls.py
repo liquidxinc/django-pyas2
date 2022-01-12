b5bf65db9dbd71c7477f8c3532b4d44b18d43313
Derek Williams <dwilliams@liquidx.com>
2022-01-12 17:39:04 -0500

    feat: Submodule update for health check

--- example/urls.py
+++ example/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("pyas2/", include('pyas2.urls')),
-    path("health", include("healht_check.urls")),
+    path("health/", include("health_check.urls")),
 ]
